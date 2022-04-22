ace2df4415356b00fae3608368e2b9496e31ac54
wcjameson <williamjameson90@gmail.com>
2022-04-22 12:55:42 -0700

    add NewKegForm component

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
+
+function NewKegForm(props) {
+
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      id: v4()
+    });
+  }
+  
+  return (
+    <React.Fragment>
+      <ReusableForm
+      formSubmissionHandler={handleNewKegFormSubmission}
+      buttonText="Submit" />
+    </React.Fragment>
+  )
+}
+
+NewKegForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default NewKegForm;
