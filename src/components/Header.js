8b52148f28a9b71307fb54ea26fedfff230f5f32
wcjameson <williamjameson90@gmail.com>
2022-04-22 09:52:13 -0700

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+
+  return(
+    <React.Fragment>
+      <h1>Welcome to the Tap Room on alpha-centauri-4</h1>
+    </React.Fragment>
+  )
+}
+
+export default Header;
