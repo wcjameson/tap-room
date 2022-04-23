3928910daaf019c2b5af7649d545b51fbd6b28a6
wcjameson <williamjameson90@gmail.com>
2022-04-22 19:57:32 -0700

    add additional styling and favicon imaga

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ function Header() {
     <React.Fragment>
       <div className="card-header">
       <h1 className="display-6">The Tap Room on Alpha-Centauri-4</h1>
-      <p className="text-lead">Welcome to your very own space beverage management tool</p>
+      <p id="welcome" className="text-lead">Welcome to your very own space beverage management tool.</p>  
+      <p id="welcome1">Keep track of your space kegs and how many space pints have been consumed!</p>
       </div>
     </React.Fragment>
   )
